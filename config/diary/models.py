ddaf40369134a1427e9b18aba796720b5d1de087
seoeunbi25 <seoeunbi25@naver.com>
2024-07-30 22:04:58 +0900

    eunbi_diary_yn

--- config/diary/models.py
+++ config/diary/models.py
@@ -5,6 +5,7 @@ from users.models import Profile
 class Diary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # 유저와의 연결
     created_at = models.DateTimeField(auto_now_add=True)  # 글 작성 시각
+    diary_yn = models.BooleanField(default=False)
     emoj = models.CharField(max_length=50)  # 이모지
     emotion1 = models.CharField(max_length=100, blank=True, null=True)
     emotion2 = models.CharField(max_length=100, blank=True, null=True)
